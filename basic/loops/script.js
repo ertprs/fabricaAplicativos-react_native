ee0b4a1c5e758943a5b2309a67e55736b05f1c79
naneraruom <naneraruom@gmail.com>
2020-12-04 09:25:49 -0300

    loops

---
+++ basic/loops/script.js
@@ -0,0 +1,17 @@
+
+i = 1;
+
+document.write("<b>While:</b>")
+while (i < 10) {
+
+    document.write("</br> The value of variable is = " + i);
+    i++;
+
+}
+
+
+document.write("</br><b>for:</b>")
+for (i = 0; i < 10; i++) {
+    document.write("</br> The value of variable is = " + i)
+
+}
