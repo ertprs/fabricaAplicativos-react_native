6d43c13f44943d73a9ebee4a08c20d81a394d1bc
naneraruom <naneraruom@gmail.com>
2020-12-04 08:56:37 -0300

    aulas iniciais

--- basic/script.js
+++ basic/script.js
@@ -1,12 +1,24 @@
 function logIn() {
-  console.log("my fuction");
-  let area = document.getElementById("area");
-  let name = prompt("What is your name?");
+    let area = document.getElementById("area");
+    let name = prompt("What is your name?");
 
-  if (name == "" || name == null) {
-    alert("retype your name");
-    area.innerHTML = "welcome";
-  } else {
-    area.innerHTML = "welcome " + name;
-  }
+    if (name == "" || name == null) {
+        alert("retype your name");
+        area.innerHTML = "welcome";
+    } else {
+        area.innerHTML = "welcome " + name;
+    }
+}
+
+function logInTwo(name) {
+    let area = document.getElementById("areaTwo");
+    let text = prompt("What is your last name?");
+
+    if(text == null || text == ""){
+        alert("retype your name");
+        area.innerHTML = "Welcome"
+    }else{
+        area.innerHTML = text + " " + name;
+    }
+   
 }
