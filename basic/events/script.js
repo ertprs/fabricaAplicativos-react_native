6f1a3d32e5261cb72e68da2e48ce330d6f9d46ca
naneraruom <naneraruom@gmail.com>
2020-12-05 15:26:06 -0300

    events

--- basic/events/script.js
+++ basic/events/script.js
@@ -1,1 +1,32 @@
+//onmousedown
+function onPressedMouse(){
+    console.log("preesed the mouse");
+}
 
+//onmouseup
+function releasedMouse(){
+    console.log("I relseaseMouse the mouse");
+}
+
+//onmouseover
+function goOver(){
+    console.log("go over the mouse");
+}
+
+//onmouseout
+function tookMouseOff(){
+    console.log("took the mouse off");
+}
+
+//onmousemove
+function moveMouse(){
+    console.log("move the mouse");
+}
+
+function onClick(){
+    console.log("onclick");
+}
+
+function onContextMenu(){
+    console.log("right the mouse");
+}
