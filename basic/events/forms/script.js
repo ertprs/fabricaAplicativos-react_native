a9ab3dac69f5ee15e70b6aea38fdacfa518ddd3b
naneraruom <naneraruom@gmail.com>
2020-12-06 21:00:04 -0300

    forms

---
+++ basic/events/forms/script.js
@@ -0,0 +1,12 @@
+function focused(){
+    console.log("focused");
+
+}
+
+function unFocused(){
+    console.log("unfocused");
+}
+
+function optionSelected(object){
+    console.log("selected city "+object.value);
+}
---
+++ test/script.js
@@ -0,0 +1,6 @@
+
+function clique(){
+   let entrada =  prompt("digite um numero?")
+
+   alert(entrada);
+}
